350cb17d936c7d77163f553a1c04ba9ec577c4ce
Tom Schimana <148782371+TomSchimana@users.noreply.github.com>
2026-09-06 13:35:32 +0200

    The tap: MMB4M as a Homebrew formula, installed from the release zips

---
+++ Formula/mmb4m.rb
@@ -0,0 +1,24 @@
+class Mmb4m < Formula
+  desc "MMBasic interpreter for macOS, ported from MMBasic for Linux"
+  homepage "https://github.com/TomSchimana/mmb4m"
+  version "0.1.1"
+  license :cannot_represent
+
+  on_arm do
+    url "https://github.com/TomSchimana/mmb4m/releases/download/v0.1.1/mmb4m-0.1.1-silicon.zip"
+    sha256 "12795eb8354fe22737cbe969d9c89b0d437aa7ebaf1f3cec7cc75674ab1280fe"
+  end
+
+  on_intel do
+    url "https://github.com/TomSchimana/mmb4m/releases/download/v0.1.1/mmb4m-0.1.1-intel.zip"
+    sha256 "8501721b62745f32599c273c2c7e6230b065a8d220a2c8b8d19934096b5ed313"
+  end
+
+  def install
+    bin.install "mmbasic"
+  end
+
+  test do
+    assert_match "MMBasic for macOS", shell_output("#{bin}/mmbasic -v")
+  end
+end
---
+++ README.md
@@ -0,0 +1,12 @@
+# TomSchimana/mmb4m
+
+A [Homebrew](https://brew.sh) tap for [MMB4M](https://github.com/TomSchimana/mmb4m), MMBasic for macOS.
+
+```sh
+brew tap TomSchimana/mmb4m
+brew install mmb4m
+```
+
+`mmbasic` works as a name for the same formula. `brew upgrade mmb4m` moves to the next version.
+
+The formula installs the binary from the MMB4M release for your processor, Apple Silicon or Intel.
